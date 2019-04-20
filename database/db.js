57a3c82f00a0ef4f8a8dcb3d5646cd15faeb2757
Jarrod <jarrod.miller@ttu.edu>
2019-04-20 11:27:53 -0500

    Added basic update/delete/post DB commands.

--- database/db.js
+++ database/db.js
@@ -26,6 +26,48 @@ const getFifteenEntries = (cb) => {
   })
 }
 
+const updatePrice = (updateObj, cb) => {
+  let { productid, productprice } = updateObj;
+  const queryString = `UPDATE PRODUCTS SET productprice = ${productprice} WHERE id=${productid}`;
+  db.query(queryString , (err, theGoods) => {
+    if (err) {
+      console.error('Error in update query.')
+      cb(err);
+    } else {
+      cb(null, theGoods);
+    }
+  });
+}
+
+const deleteProduct = (productid, cb) => {
+  const queryString = `DELETE FROM products WHERE id=${productid}`;
+  db.query(queryString , (err, theGoods) => {
+    if (err) {
+      console.error('Error in update query.')
+      cb(err);
+    } else {
+      cb(null, theGoods);
+    }
+  });
+}
+
+const postProduct = (product, cb) => {
+  let { name, price, type} = product;
+  let url = 'https://s3.amazonaws.com/fecphotogallery2019/photos/48_3.jpg';
+  const queryString = `INSERT INTO products (productname, productpic, productprice, producttype) VALUES ('${name}', '${url}', ${price}, '${type}');`;
+  console.log('Query: ', queryString);
+  db.query(queryString, (err, theGoods) => {
+    if (err) {
+      console.error('Error in POST query. ', err);
+      cb(err);
+    } else {
+      cb(null, theGoods);
+    }
+  })
+
+}
+
+
 module.exports = {
-  getFifteenEntries
+  getFifteenEntries, updatePrice, deleteProduct, postProduct 
 };
--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const app = express();
 const PORT = 4444;
 const db = require('../database/db.js');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 app.use(cors());
 app.use(express.static('dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/products', (req, res) => {
   //usually the req would have a search term, like * to query the db to get all
@@ -14,6 +17,23 @@ app.get('/api/products', (req, res) => {
     console.log(data)
     res.send(data);
   })
+})
+
+app.post('/api/products', (req, res) => {
+  //const queryString = `INSERT INTO products (productname, productpic, productprice, producttype) VALUES ('${name}', '${url}', ${price}, ${type});`;
+  console.log('Received request for', req.body)
+  let product = { name, price, type } = req.body;
+  if (!product.name || product.name.length > 6 || !product.price || !product.type) {
+    res.status(400);
+    res.end('Improper parameters.');
+  }
+  else {
+    db.postProduct(product, (err, data) => {
+      res.status(201);
+      res.end();
+    })
+    res.end();
+  }
 })
 
 app.listen(PORT, () => {
